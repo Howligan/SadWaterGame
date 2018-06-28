ef67e76fd089a99b14ea9e73fb627bf5776d9b44
Howligan <redrebel62@gmail.com>
2018-06-28 18:35:20 -0400

    First commit
    Game has:
    Moving player
    Single falling object for testing
    Player boundaries
    
    Game needs:
    Spawner for falling objects
    Score counter
    
    *needs indicate immediate tasks. not indicative of final project*

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField]
+    //Variables
+    public int speed;
+
+    // Update is called once per frame
+
+    void Update () {
+        TranslationMovement();
+        PlayerBounds();
+	}
+
+    void TranslationMovement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(new Vector2(-speed, 0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(new Vector2(speed, 0) * Time.deltaTime);
+        }
+    }
+
+    void PlayerBounds()
+    {
+        if(transform.position.x <= -8.355783f )
+        {
+            this.transform.position = new Vector2( -8.355783f, transform.position.y);
+        }
+
+        if (transform.position.x >= 8.355783f)
+        {
+            this.transform.position = new Vector2(8.355783f, transform.position.y);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "CleanWater")
+        {
+            Destroy(col.gameObject);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/WaterDropBrains.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDropBrains : MonoBehaviour {
+    //Variables
+    [SerializeField]
+    public int fallSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+        Falling();
+	}
+
+    void Falling()
+    {
+        this.transform.Translate(new Vector2(0, -fallSpeed) * Time.deltaTime);
+    }
+}
