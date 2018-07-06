5583b1135d42f9a3100e892dd8d4124c656e439d
Howligan <redrebel62@gmail.com>
2018-07-05 23:56:04 -0400

    Changes 7/5/18
    Edited Player collider to include bottle
    Removed rigidbodies from tears
    Added placeholder crate and truck
    Added 3 bottles to player
    Added water catching system (see playermovement.cs)
    
    Immediate Notes:
    Need to resize play area
    Need to add score counter
    Need to add game over conditions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,47 @@ public class PlayerMovement : MonoBehaviour {
     //Variables
     private int speed;
     public GameObject gameManager;
+    public Rigidbody2D playerRbody;
+    public bool playerHasBottle;
+    public int playerWaterCount;
+
+    public GameObject emptyBottle;
+    public GameObject bottle1;
+    public GameObject bottle2;
+    public GameObject bottle3;
+
+    public enum PlayerState { NoBottle, BottleEmpty, Bottle1, Bottle2, BottleFull}
+    public PlayerState currentPlayerState;
+
+    void Start()
+    {
+        playerRbody = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
 
     void Update () {
         TranslationMovement();
         PlayerBounds();
+
+        switch (currentPlayerState)
+        {
+            case PlayerState.NoBottle:
+                PlayerHasNoBottle();
+                break;
+            case PlayerState.BottleEmpty:
+                Player03Bottle();
+                break;  
+            case PlayerState.Bottle1:
+                Player13Bottle();
+                break;
+            case PlayerState.Bottle2:
+                Player23Bottle();
+                break;
+            case PlayerState.BottleFull:
+                Player33Bottle();
+                break;
+        }
 	}
 
     void TranslationMovement()
@@ -45,16 +80,99 @@ public class PlayerMovement : MonoBehaviour {
     {
         if (col.gameObject.tag == "CleanWater")
         {
-            gameManager.GetComponent<GameManager>().playerScore += 10;
-            Destroy(col.gameObject);
+            if (currentPlayerState == PlayerState.BottleEmpty)
+            {
+                currentPlayerState = PlayerState.Bottle1;
+                Destroy(col.gameObject);
+            }
+            else if (currentPlayerState == PlayerState.Bottle1)
+            {
+                currentPlayerState = PlayerState.Bottle2;
+                Destroy(col.gameObject);
+            }
+            else if (currentPlayerState == PlayerState.Bottle2)
+            {
+                currentPlayerState = PlayerState.BottleFull;
+                Destroy(col.gameObject);
+            }
         }
 
         if (col.gameObject.tag == "DirtyWater")
         {
-            gameManager.GetComponent<GameManager>().playerHP--;
+            currentPlayerState = PlayerState.NoBottle;
             Destroy(col.gameObject);
         }
 
+        if (col.gameObject.tag == "Crate")
+        {
+            if(playerHasBottle == false && playerWaterCount == 0)
+            {
+                currentPlayerState = PlayerState.BottleEmpty;
+            }
+        }
 
+        if(col.gameObject.tag == "Truck")
+        {
+            if(playerHasBottle == true && playerWaterCount == 3)
+            {
+                currentPlayerState = PlayerState.NoBottle;
+            }
+        }
+    }
+
+    void PlayerHasNoBottle()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = true;
+        playerHasBottle = false;
+        playerWaterCount = 0;
+        emptyBottle.SetActive(false);
+        bottle1.SetActive(false);
+        bottle2.SetActive(false);
+        bottle3.SetActive(false);
+    }
+
+    void Player03Bottle()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = false;
+
+        playerHasBottle = true;
+        playerWaterCount = 0;
+        emptyBottle.SetActive(true);
+        bottle1.SetActive(false);
+        bottle2.SetActive(false);
+        bottle3.SetActive(false);
+    }
+
+    void Player13Bottle()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        playerHasBottle = true;
+        playerWaterCount = 1;
+        emptyBottle.SetActive(false);
+        bottle1.SetActive(true);
+        bottle2.SetActive(false);
+        bottle3.SetActive(false);
+    }
+
+    void Player23Bottle()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        playerHasBottle = true;
+        playerWaterCount = 2;
+        emptyBottle.SetActive(false);
+        bottle1.SetActive(false);
+        bottle2.SetActive(true);
+        bottle3.SetActive(false);
+    }
+
+    void Player33Bottle()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = true;
+        playerHasBottle = true;
+        playerWaterCount = 3;
+        emptyBottle.SetActive(false);
+        bottle1.SetActive(false);
+        bottle2.SetActive(false);
+        bottle3.SetActive(true);
     }
 }
